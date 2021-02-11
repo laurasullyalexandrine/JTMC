353c22d3ddb7d82993228cb3b0e5ffc9150c06c7
Guy Guyon <guyon.guy@sfr.fr>
2021-02-11 15:47:46 +0100

    create entity commercial service

---
+++ migrations/Version20210211144506.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210211144506 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commercial_service (id INT AUTO_INCREMENT NOT NULL, relais_colis TINYINT(1) DEFAULT NULL, mondial_relais TINYINT(1) DEFAULT NULL, click_and_collect TINYINT(1) DEFAULT NULL, too_go_to_go TINYINT(1) DEFAULT NULL, delivery TINYINT(1) DEFAULT NULL, to_take_away TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE commercial_service');
+    }
+}
---
+++ src/Entity/CommercialService.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommercialServiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommercialServiceRepository::class)
+ */
+class CommercialService
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $relais_colis;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $mondial_relais;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $click_and_collect;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $too_goog_to_go;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $delivery;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $to_take_away;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRelaisColis(): ?bool
+    {
+        return $this->relais_colis;
+    }
+
+    public function setRelaisColis(?bool $relais_colis): self
+    {
+        $this->relais_colis = $relais_colis;
+
+        return $this;
+    }
+
+    public function getMondialRelais(): ?bool
+    {
+        return $this->mondial_relais;
+    }
+
+    public function setMondialRelais(?bool $mondial_relais): self
+    {
+        $this->mondial_relais = $mondial_relais;
+
+        return $this;
+    }
+
+    public function getClickAndCollect(): ?bool
+    {
+        return $this->click_and_collect;
+    }
+
+    public function setClickAndCollect(?bool $click_and_collect): self
+    {
+        $this->click_and_collect = $click_and_collect;
+
+        return $this;
+    }
+
+    public function getTooGoodToGo(): ?bool
+    {
+        return $this->too_good_to_go;
+    }
+
+    public function setTooGoodToGo(?bool $too_good_to_go): self
+    {
+        $this->too_good_to_go = $too_good_to_go;
+
+        return $this;
+    }
+
+    public function getDelivery(): ?bool
+    {
+        return $this->delivery;
+    }
+
+    public function setDelivery(?bool $delivery): self
+    {
+        $this->delivery = $delivery;
+
+        return $this;
+    }
+
+    public function getToTakeAway(): ?bool
+    {
+        return $this->to_take_away;
+    }
+
+    public function setToTakeAway(?bool $to_take_away): self
+    {
+        $this->to_take_away = $to_take_away;
+
+        return $this;
+    }
+}
