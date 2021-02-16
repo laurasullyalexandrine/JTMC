4578940005933f2d934239decb6cb4362e58359f
Guy Guyon <guyon.guy@sfr.fr>
2021-02-16 20:34:00 +0100

    Create form + start creating StoreController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,19 @@
 
 namespace App\Controller;
 
-/**
- * @Route("/home", name="homepage")
- */
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-class MainController extends AbstractController
+/**
+ * @Route("/home")
+ */
+ class MainController extends AbstractController
     {
         /**
-         * @Route("/", name="homepage", methods="GET")
+         * @Route("/", name="home", methods="GET")
          */
         public function homepage (): Response
         {
-            return $this->render('main/homepage.html.twig');
+            return $this->render('main/home.html.twig');
         }
     }
