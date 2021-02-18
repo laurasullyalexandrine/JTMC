cf03502b2ef0aa0bf5ba22e9d30d72e44bd07982
laurasyllyalexandrine <launyie@gmail.com>
2021-02-19 00:44:26 +0100

    add code html for home store and legals mentions

--- public/js/menu-burger.js
+++ public/js/menu-burger.js
@@ -1,33 +1,8 @@
 
-let left = document.querySelector('#menu-search');
-let sidebarBody = document.querySelector('#hamburger-sidebar-body');
-let button = document.querySelector('#hamburger-button');
-let overlay = document.querySelector('#hamburger-overlay');
-let activatedClass = 'hambuger-activated';
-console.log('Hello Webpack Encore !');
+let btn = document.querySelector('.toggle_btn');
+let nav = document.querySelector('.nav');
 
-sidebarBody.innerHTML = left.innerHTML;
-
-button.addEventListener('click', function(e)
+btn.onclick = function()
 {
-    e.preventDefault();
-   this.parentNode.classList.add(activatedClass);
-});
-
-button.addEventListener('click', function(e)
-{
-    if(this.parentNode.classList.contains(activatedClass))
-    {
-        if (e.repeat === false)
-        {
-            this.parentNode.classList.remove(activatedClass);
-        }
-    }
-});
-
-// on se sert de notre var. overlay pour dans un premier temps 
-overlay.addEventListener('click', function(e)
-{
-    e.preventDefault();
-    this.parentNode.classList.remove(activatedClass);
-});
+    nav.classList.toggle('nav_open');
+}
