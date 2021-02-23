a5b0fdccbf99f333fe0fc2574b6419f3709d60a6
laurasyllyalexandrine <launyie@gmail.com>
2021-02-23 14:31:27 +0100

    update project

--- migrations/Version20210223130322.php
+++ migrations/Version20210223130322.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210223123315 extends AbstractMigration
+final class Version20210223130322 extends AbstractMigration
 {
     public function getDescription() : string
     {
