e7d6e22ce0039dfe81c3325d7dedf3c59a710cf0
laurasyllyalexandrine <launyie@gmail.com>
2021-04-12 14:43:57 +0200

    modification of the css of the web and mobile format page and adaptation of the hamburger-button  file

--- public/js/hamburger-button.js
+++ public/js/hamburger-button.js
@@ -1,20 +1,37 @@
 
 // console.log('Hello !')
-// Here we get the hamburger-content and hamburger-sidebar-body 'id' in the DOM
-let btn = document.querySelector('.hamburger-button');
-let nav = document.querySelector('.nav');
-// let content = document.querySelector('.hamburger-content');
-// let sidebarBody = document.querySelector('.hamburger-sidebar-body');
-// let searchEngine = document.querySelector('.search-engine');
-// let sidebarHeader = document.querySelector('.hamburger-sidebar-header');
+// here we create many variables to then build our function js
 
+let searchEngine = document.querySelector('.search-engine');
+let sidebarHeader = document.querySelector('.hamburger-sidebar-header');
+
+let content = document.querySelector('.hamburger-content');
+let sidebarBody = document.querySelector('.hamburger-sidebar-body');
+
+let btn = document.querySelector('.hamburger-button');
+let sidebar = document.querySelector('.hamburger-sidebar');
+let overlay = document.querySelector('.hamburger-overlay');
 
 
 // Here the hamburger-sidebar-body id will replace the hamburger-content id same think for search-engine id with sidebar-header id. 
-// sidebarBody.innerHTML = content.innerHTML;
-// sidebarHeader.innerHTML = searchEngine.innerHTML;
+sidebarHeader.innerHTML = searchEngine.innerHTML;
+sidebarBody.innerHTML = content.innerHTML;
 
-btn.onclick = function()
-{
-    nav.classList.toggle('nav-open');
+
+// Here we create two functions that make it possible to boost the responsive interface
+btn.onclick = function() {
+    sidebar.classList.toggle('sidebar-open');
+    overlay.classList.toggle('overlay-open');
+    // console.log(btn);
 }
+
+
+overlay.onclick = function() {
+    if(overlay)
+    {
+        sidebar.classList.remove('sidebar-open');
+        overlay.classList.remove('overlay-open');
+    }
+    console.log(overlay);
+}
+ 
