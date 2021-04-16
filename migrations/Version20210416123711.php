6d230ca80df343de36d88975cf3a2a669d3184da
laurasullyalexandrine <launyie@gmail.com>
2021-04-16 15:29:45 +0200

    modification of the environment of dev migration bdd on phpmyadmin and modification .env

--- migrations/Version20210416123711.php
+++ migrations/Version20210416123711.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210226143810 extends AbstractMigration
+final class Version20210416123711 extends AbstractMigration
 {
     public function getDescription() : string
     {
